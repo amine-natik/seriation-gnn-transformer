30a2c2e1df2eeaa6939074844e90193966302033
Amine Natik <37818410+amine-natik@users.noreply.github.com>
2021-11-30 18:19:02 -0500

    Update and rename cora.py to planetoid.py

--- dataloaders/planetoid.py
+++ dataloaders/planetoid.py
@@ -1,17 +1,19 @@
 from torch_geometric.datasets import Planetoid
 from utils.preprocessing import label_ordering, random_ordering, no_ordering, laplacian_ordering
 
-def load_graph(cfg):
-    dataset = Planetoid(root=cfg.data_root, name=cfg.data)
+
+def load_graph(data_root, data_name):
+    dataset = Planetoid(root=data_root, name=data_name)
     graph = dataset[0]
     return graph
 
-def process_graph(graph, process_method, save_dir=None):
+
+def process_graph(graph, process_method, save_dir=None, idx=None):
     if process_method == 'no_ordering':
         permutation = no_ordering(graph)
 
     elif process_method == 'laplacian_ordering':
-        permutation = laplacian_ordering(graph, save_dir)
+        permutation = laplacian_ordering(graph, save_dir, idx)
 
     elif process_method == 'label_ordering':
         permutation = label_ordering(graph)
