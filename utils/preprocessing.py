4ef460780c89dc213d9364c02e0937f1cab87954
Amine Natik <37818410+amine-natik@users.noreply.github.com>
2021-11-30 18:22:32 -0500

    Update preprocessing.py

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -3,34 +3,13 @@ from torch_geometric.utils import get_laplacian, to_scipy_sparse_matrix
 from scipy.sparse.linalg import eigsh
 import os
 from pathlib import Path
-from sklearn.cluster import SpectralClustering
-from torch_geometric.utils import to_dense_adj
-
-
-def sc_ordering(graph, save_dir):
-    save_dir = Path(save_dir)
-    if not save_dir.exists():
-        save_dir.mkdir()
-    save_path = save_dir.joinpath('sc_predictions.pt')
-
-    if os.path.isfile(save_path):
-        predictions = torch.load(save_path)
-    else:
-        adjacency = to_dense_adj(graph.edge_index).squeeze(0)
-        sc = SpectralClustering(n_clusters=7, affinity='precomputed', assign_labels='discretize')
-        sc.fit(adjacency)
-        predictions = torch.tensor(sc.labels_)
-        torch.save(predictions, save_path)
-
-    permutation = torch.argsort(predictions)
-    return permutation
 
 
 def laplacian_ordering(graph, save_dir, idx):
     save_dir = Path(save_dir)
     if not save_dir.exists():
         save_dir.mkdir()
-    save_path = save_dir.joinpath('.eigenvectors.pt')
+    save_path = save_dir.joinpath('eigenvectors.pt')
 
     if os.path.isfile(save_path):
         eigenvectors = torch.load(save_path)
